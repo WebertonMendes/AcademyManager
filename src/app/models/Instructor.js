68f5e07d5f08a2187f62bb7595264d53b0eb7203
WebertonMendes <weberton.mendes@hotmail.com>
2020-05-24 07:09:33 -0300

    GymManager

--- src/app/models/Instructor.js
+++ src/app/models/Instructor.js
@@ -3,11 +3,23 @@ const db = require('../../config/db')
 
 module.exports = {
     all(callback) {
-        db.query(`SELECT * FROM instructors ORDER BY name ASC`, function(err, results){
-            if(err) throw `Database Error! ${err}`
+        db.query(`
+            SELECT
+                instructors.*,
+                COUNT(members) AS total_students
+            FROM
+                instructors
+            LEFT JOIN
+                members ON (members.instructor_id = instructors.id)
+            GROUP BY
+                instructors.id
+                ORDER BY total_students`, 
+            function(err, results){
+                if(err) throw `Database Error! ${err}`
         
-            callback(results.rows)
-        })
+                callback(results.rows)
+            }
+        )
     },
     create(data, callback) {
         
@@ -45,6 +57,22 @@ module.exports = {
                 if(err) throw `Database Error! ${err}`
                 callback(results.rows[0])
             })
+    },
+    findBy(filter, callback) {
+        db.query(`
+            SELECT instructors.*, COUNT(members) AS total_students
+            FROM instructors
+            LEFT JOIN members ON (members.instructor_id = instructors.id)
+            WHERE instructors.name ILIKE '%${filter}%'
+            OR instructors.services ILIKE '%${filter}%'
+            GROUP BY instructors.id
+            ORDER BY total_students`, 
+            function(err, results){
+                if(err) throw `Database Error! ${err}`
+        
+                callback(results.rows)
+            }
+        )
     },
     update(data, callback) {
         const query = `
--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -20,8 +20,9 @@ module.exports = {
                 gender,
                 blood,
                 weight,
-                height
-            ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+                height,
+                instructor_id
+            ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
             RETURNING id`
 
         const values = [
@@ -32,7 +33,8 @@ module.exports = {
             data.gender,
             data.blood,
             data.weight,
-            data.height
+            data.height,
+            data.instructor
         ]
 
         db.query(query, values, function(err, results){
@@ -43,10 +45,12 @@ module.exports = {
     },
     find(id, callback) {
         db.query(`
-            SELECT *
+            SELECT members.*, instructors.name AS instructor_name
             FROM members
-            WHERE id = $1`, [id], function(err, results) {
+            LEFT JOIN instructors ON (members.instructor_id = instructors.id)
+            WHERE members.id = $1`, [id], function(err, results) {
                 if(err) throw `Database Error! ${err}`
+                
                 callback(results.rows[0])
             })
     },
@@ -60,8 +64,9 @@ module.exports = {
                 gender=($5),
                 blood=($6),
                 weight=($7),
-                height=($8)
-            WHERE id = $9`
+                height=($8),
+                instructor_id=($9)
+            WHERE id = $10`
         
         const values = [
             data.avatar_url,
@@ -72,13 +77,14 @@ module.exports = {
             data.blood,
             data.weight,
             data.height,
+            data.instructor,
             data.id
         ]
-
+        
         db.query(query, values, function(err, results) {
             if(err) throw `Database Error! ${err}`
 
-            return callback()
+            callback()
         })
     },
     delete(id, callback) {
@@ -89,5 +95,12 @@ module.exports = {
                 return callback()
             }
         )
+    },
+    instructorsSelectOptions(callback) {
+        db.query(`SELECT name, id FROM instructors`, function(err, results) {
+            if(err) throw `Database Error! ${err}`
+
+            callback(results.rows)
+        })
     }
 }
