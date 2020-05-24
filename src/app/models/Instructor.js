5fe593434785b51c882a1305820f1536de44bf40
WebertonMendes <weberton.mendes@hotmail.com>
2020-05-24 18:33:53 -0300

    GymManager

--- src/app/models/Instructor.js
+++ src/app/models/Instructor.js
@@ -4,19 +4,13 @@ const db = require('../../config/db')
 module.exports = {
     all(callback) {
         db.query(`
-            SELECT
-                instructors.*,
-                COUNT(members) AS total_students
-            FROM
-                instructors
-            LEFT JOIN
-                members ON (members.instructor_id = instructors.id)
-            GROUP BY
-                instructors.id
-                ORDER BY total_students`, 
+            SELECT instructors.*, COUNT(members) AS total_students
+            FROM instructors
+            LEFT JOIN members ON (members.instructor_id = instructors.id)
+            GROUP BY instructors.id
+            ORDER BY instructor.name`,
             function(err, results){
                 if(err) throw `Database Error! ${err}`
-        
                 callback(results.rows)
             }
         )
